cadab98a72a2eeae838666a9833326a1f1d6f884
cyrus-raitava <crai897@aucklanduni.ac.nz>
2019-04-06 17:37:05 +1300

    Uncomment tests, leave gradle build script as it is

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -3,25 +3,25 @@ import org.junit.Test;
 
 public class DollarTest {
 
-//    @Test
-//    public void testDollarDivision() {
-//        // Given
-//        Dollar ten = new Dollar(10);
-//
-//        // When
-//        Dollar result = ten.dividedBy(2);
-//
-//        // Then
-//        assertEquals(new Dollar(5), result);  // Change to 4 to see the failure
-//    }
+    @Test
+    public void testDollarDivision() {
+        // Given
+        Dollar ten = new Dollar(10);
 
-//    @Test(expected=ArithmeticException.class)
-//    public void testDollarDividedByZero() {
-//        // Given
-//        Dollar ten = new Dollar(10);
-//
-//        // When
-//        ten.dividedBy(0);
-//    }
+        // When
+        Dollar result = ten.dividedBy(2);
+
+        // Then
+        assertEquals(new Dollar(5), result);  // Change to 4 to see the failure
+    }
+
+    @Test(expected=ArithmeticException.class)
+    public void testDollarDividedByZero() {
+        // Given
+        Dollar ten = new Dollar(10);
+
+        // When
+        ten.dividedBy(0);
+    }
 
 }
